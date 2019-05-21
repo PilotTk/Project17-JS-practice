118300397f3ee207362681f8bffa274b593b0571
PilotTk <taavi.kirss91@gmail.com>
2019-05-21 16:46:47 +0300

    Updated files list

--- exerciseES7.js
+++ exerciseES7.js
@@ -1,26 +1,16 @@
-// Solve the below problems:
-
-
-// #1) Check if this array includes the name "John".
-const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-
-dragons.includes("John");
-
-// #2) Check if this array includes any name that has "John" inside of it. If it does, return that
-// name or names in an array.
-const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-
-
-// #3) Create a function that calulates the power of 100 of a number entered as a parameter
+//Function that calulates the power of 100 of a number 
+// entered as a parameter
 
 var hundred() => (x) => Math.pow(x, 100)
 
-// #4) Useing your function from #3, put in the paramter 10000. What is the result?
-// Research for yourself why you get this result
+// with parameter 10000. 
 
 function hundred(x) {
 	return Math.pow(x, 10000);
 }
+
+
+//Some practice 
 
 let obj = {
 	username0: "Santa",
